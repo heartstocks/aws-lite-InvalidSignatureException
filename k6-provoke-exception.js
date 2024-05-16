686f5e81ef4bd8e96211501316d782add381f836
Alex Bepple <alex@bepple.de>
2024-05-16 15:35:24 +0200

    % pass target url from command line

--- k6-provoke-exception.js
+++ k6-provoke-exception.js
@@ -1,7 +1,7 @@
 import http from 'k6/http'
 
 export default function () {
-    const res = http.get("…");
+    const res = http.get(__ENV.TARGET) // invoke using `k6 --env TARGET="https://example.com"`
     if (res.status !== 200) {
         console.log(res.status_text)
     }
