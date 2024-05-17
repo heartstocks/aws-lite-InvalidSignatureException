4490976d2285d9fc0f60ac8937373bf1fa509f1b
Alex Bepple <alex@bepple.de>
2024-05-17 22:28:50 +0200

    % reduce to the essence

--- src/http/get-index/index.mjs
+++ src/http/get-index/index.mjs
@@ -1,7 +1,6 @@
 import arc from "@architect/functions";
 
 export async function handler (req) {
-  const client = await arc.tables()
-  const things = client.things
-  return things.scanAll()
+  // add > 20 SSM params in order to be easily reproducible with `k6 --vus 200 --iterations 200 …`
+  await arc.services()
 }
